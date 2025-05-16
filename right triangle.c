9a88610b0c43d628a8855cd6a69a53d723bc5f1f
Hetvi <hetvi3304@gmail.com>
2025-05-16 17:02:30 +0530

    first step

---
+++ right triangle.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    int third_angle;
+    int first_angle=65;
+    int second_angle=45;
+    third_angle=180-(first_angle + second_angle);
+    printf("Third angle of right angle triangle:%d",third_angle);
+
+}
